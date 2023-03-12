bfd1066a3a9bbc631fd085c137fbb9eeb9725b99
Sanambir Singh <sanambir123@gmail.com>
2023-03-12 20:16:21 +0530

    Create route and controller for login

--- controllers/api/v1/doctorsController.js
+++ controllers/api/v1/doctorsController.js
@@ -1,10 +1,17 @@
 const Doctor = require('../../../models/doctor'); // import doctor model
 
+// variables for jwt token
+const jwt = require('jsonwebtoken');
+const SECRETKEY = 'hospital-api';
+
+// import passport
+const passport = require('passport');
+
 // controller to register a doctor
 module.exports.create = async function(req, res){
     console.log(req.body);
     try{
-        const doctor = await Doctor.create(req.body);
+        const doctor = await Doctor.create({...req.body, email: req.body.email.toLowerCase()});
         return res.status(200).json({
             message: "Doctor registered successfully",
             doctor: doctor
@@ -16,3 +23,42 @@ module.exports.create = async function(req, res){
         });
     }
 }
+
+// controller to login a doctor
+module.exports.createSession = async function(req, res){
+    // find the doctor
+    try{
+        const doctor = await Doctor.findOne(req.body);
+        // if doctor not found
+        if (!doctor){
+            return res.status(422).json({
+                message: "Invalid username or password"
+            });
+        }
+        // define pay load data
+        const payload = {
+            _id: doctor._id,
+            name: doctor.name,
+            email: doctor.email,
+            exp: Math.floor(Date.now() / 1000) + (60) // Token expiration time
+
+        }
+        // generate JWT token
+        const token = jwt.sign(payload, SECRETKEY);
+
+        // send the token to the client
+        return res.status(200).json({
+            message: "Sign in successful, here is your token, please keep it safe!",
+            data: {
+                token
+            }
+        });
+    }catch(err){
+        console.log('Error', err);
+        return res.status(500).json({
+            message: "Internal Server Error"
+        });
+    }
+}
+
+
--- routes/api/v1/doctors.js
+++ routes/api/v1/doctors.js
@@ -8,4 +8,7 @@ const doctorController = require('../../../controllers/api/v1/doctorsController'
 // route to register a doctor
 router.post('/register', doctorController.create);
 
+// route to login a doctor
+router.post('/login', doctorController.createSession);
+
 module.exports = router;
